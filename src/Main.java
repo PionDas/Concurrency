a5b6c5b62f2369c4d6473fb12559f48ce2c7ba92
PionDas <40925743+PionDas@users.noreply.github.com>
2020-12-25 23:42:15 -0500

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //threads are useful to run multiple pieces of code at the same time
-        
+
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
